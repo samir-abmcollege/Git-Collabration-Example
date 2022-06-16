8fc603e5ec9cdbd5cc042774f28d6fb82b7cf68e
Jordan Peralta <jordanpaulperalta@yahoo.com>
2022-06-15 19:52:14 -0600

    ABM-03_Added functionality for the camera button

--- Git-Collabration-Example/ViewController.swift
+++ Git-Collabration-Example/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import PhotosUI
 
 class ViewController: UIViewController {
 
@@ -13,11 +14,51 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         navigationItem.rightBarButtonItems = [
-            UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: nil),
+            UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(showAlbum)),
             UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: nil)
         ]
+
     }
-
-
+    
+    @objc func showAlbum() {
+        var config = PHPickerConfiguration()
+        config.selectionLimit = 1
+        config.filter = PHPickerFilter.images
+        
+        let pickerVC = PHPickerViewController(configuration: config)
+        pickerVC.delegate = self
+        self.present(pickerVC, animated: true, completion: nil)
+        
+        
+    }
 }
 
+extension ViewController: PHPickerViewControllerDelegate {
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        picker.dismiss(animated: true)
+        
+        guard let provider = results.first?.itemProvider else {
+            return
+        }
+        
+        if provider.canLoadObject(ofClass: UIImage.self) {
+            provider.loadObject(ofClass: UIImage.self) { [weak self] maybeImage, maybeError in
+                guard let image = maybeImage as? UIImage,
+                      maybeError == nil,
+                      let imageData = image.pngData() else {
+                    print("ERROR: \(String(describing: maybeError))")
+                    return
+                }
+            
+                DispatchQueue.main.async {
+                let pushImage = ImageVC()
+                pushImage.getImage = image
+                guard let vc = self?.storyboard?.instantiateViewController(identifier: "showImage") as? ImageVC else {
+                    return
+            }
+                self?.navigationController?.pushViewController(vc, animated: true)
+                }
+        }
+    }
+    }
+}
