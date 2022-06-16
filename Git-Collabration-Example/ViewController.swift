79f7b2f7e4f08a8c739b9e3a5bcc34c6ddae26e9
radiney <99511595+radiney@users.noreply.github.com>
2022-06-16 09:42:00 +0800

    ABM_04_lifecyclesOverrides

--- Git-Collabration-Example/ViewController.swift
+++ Git-Collabration-Example/ViewController.swift
@@ -6,13 +6,40 @@
 //
 
 import UIKit
+import SafariServices
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+	title = "Collaboration"
         // Do any additional setup after loading the view.
+
+	//this is for abm04 nvigation and safari test
     }
+
+override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        print("viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("s viewDidAppear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        print("viewWillDisappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        print("viewDidDisappear")
+    }	
 
 
 }
