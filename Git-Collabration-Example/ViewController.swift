ea533120d62f5c07f99201bd1693ccc41935a826
Samir Anghan <samir.anghan@gmail.com>
2022-06-17 20:15:58 -0400

    ABM-07: Added image upload button on main screen

--- Git-Collabration-Example/ViewController.swift
+++ Git-Collabration-Example/ViewController.swift
@@ -11,10 +11,17 @@ import PhotosUI
 class ViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var uploadImageButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         imageView.image = UIImage(named: "work")
+        uploadImageButton.setTitle("Upload Image", for: .normal)
     }
+    
+    @IBAction func didTapImageUpload(_ sender: UIButton) {
+        print("Image upload button tapped")
+    }
+    
 }
